0e465d9ba13a46ab951f0ae4353cc6ffe110903a
unknown <Alumno@DESKTOP-0NO01OC.examenes.net>
2020-03-10 10:55:42 +0100

    Modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainModule } from './main/main.module';
+import { M2iCoreModule } from 'src/m2i-core';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, FormsModule,
+    AppRoutingModule, MainModule, M2iCoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/m2i-core/index.ts
@@ -0,0 +1,1 @@
+export * from './m2i-core.module';
