b5788d43ea4929c739e09acbb67b8e4b13d98d83
unknown <Alumno@DESKTOP-0NO01OC.examenes.net>
2020-03-10 12:15:33 +0100

    Logger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoggerService } from 'src/m2i-core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Hola Mundo';
+
+  constructor(private out: LoggerService) {}
+
+  ngOnInit(): void {
+    this.out.error('Esto es un error');
+    this.out.warn('Esto es un warn');
+    this.out.info('Esto es un info');
+    this.out.log('Esto es un log');
+  }
 }
--- src/m2i-core/index.ts
+++ src/m2i-core/index.ts
@@ -1,1 +1,2 @@
 export * from './m2i-core.module';
+export * from './services/logger.service';
