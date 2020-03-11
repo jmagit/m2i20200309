d29fd3f62957701c99fbfa07610add72cad10372
unknown <Alumno@DESKTOP-0NO01OC.examenes.net>
2020-03-11 12:58:18 +0100

    Dinamico

--- src/app/dinamico/dinamico.component.ts
+++ src/app/dinamico/dinamico.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeComponent } from '../home/home.component';
+import { DemosComponent } from '../demos/demos.component';
 
 @Component({
   selector: 'app-dinamico',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dinamico.component.css']
 })
 export class DinamicoComponent implements OnInit {
+  menu = [
+    { texto: 'inicio', componente: HomeComponent },
+    { texto: 'demos', componente: DemosComponent },
+  ];
+  seleccionado = this.menu[0].componente;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  seleccionar(indice: number) {
+    this.seleccionado = this.menu[indice].componente;
+  }
 }
